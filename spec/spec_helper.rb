60141098b8dece08c0dd9abb7207b95349367e91
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2012-04-21 13:59:01 -0300

    Move rspec matchers to support/matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,4 @@
 require 'range_sentence_parser'
 require 'range_sentence_parser/invalid_sentence_error'
 
-RSpec::Matchers.define :be_parsed_to do |expected|
-  match do |actual|
-    RangeSentenceParser.parse!(actual) == expected
-  end
-end
-
-RSpec::Matchers.define :be_a_valid_sentence do |expected|
-  match do |actual|
-    RangeSentenceParser.new(actual).valid?
-  end
-end
-
-RSpec::Matchers.define :be_an_invalid_sentence do |expected|
-  match do |actual|
-    RangeSentenceParser.new(actual).invalid?
-  end
-end
+require 'support/matchers'
---
+++ spec/support/matchers.rb
@@ -0,0 +1,17 @@
+RSpec::Matchers.define :be_parsed_to do |expected|
+  match do |actual|
+    RangeSentenceParser.parse!(actual) == expected
+  end
+end
+
+RSpec::Matchers.define :be_a_valid_sentence do |expected|
+  match do |actual|
+    RangeSentenceParser.new(actual).valid?
+  end
+end
+
+RSpec::Matchers.define :be_an_invalid_sentence do |expected|
+  match do |actual|
+    RangeSentenceParser.new(actual).invalid?
+  end
+end
